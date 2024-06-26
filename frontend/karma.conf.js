911df9a16abb06ec514e7bb733941a0a3b9958cc
diegomezalinas <dalejandrogs537@gmail.com>
2024-06-25 20:07:05 -0500

    se agrego linea 51 en karma

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -48,6 +48,7 @@ module.exports = function (config) {
       ChromeHeadless: {
           base: 'Chrome',
           flags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']
+          binary: process.env.CHROME_BIN
       }
     },
     singleRun: true
